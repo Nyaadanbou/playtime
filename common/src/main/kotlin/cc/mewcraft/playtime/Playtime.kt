0ad1433dd5ed1d6406563cc2d7367927673869db
Akiranya <porridgen@gmail.com>
2024-11-11 17:15:00 +0800

    more robust coroutine implementations

--- common/src/main/kotlin/cc/mewcraft/playtime/Playtime.kt
+++ common/src/main/kotlin/cc/mewcraft/playtime/Playtime.kt
@@ -1,26 +1,28 @@
 package cc.mewcraft.playtime
 
 import cc.mewcraft.playtime.data.PlaytimeData
-import java.util.*
+import java.util.UUID
 
 /**
- * 提供获取玩家数据的接口.
+ * 提供用于管理 [PlaytimeData] 的接口.
+ *
+ * 使用 [PlaytimeProvider] 来获取本接口的实例.
  */
 interface Playtime {
     /**
      * 获取玩家的游戏时间数据.
+     * 当获取到的数据为 `null`, 表示玩家还没有游戏时间数据.
+     * 这可能是因为玩家刚进入服务器后还未到数据的刷新时间.
      *
-     * 当获取到的数据为 null, 表示玩家没有游戏时间数据. (可能是玩家进入服务器未满刷新时间)
-     *
-     * @param uuid 玩家的 UUID.
+     * @param uuid 玩家的 UUID
      */
     suspend fun getPlaytime(uuid: UUID): PlaytimeData?
 
     /**
-     * 设置玩家的游戏时间数据.
+     * 设置玩家的游戏时间数据, 将覆盖原有的数据.
      *
-     * @param uuid 玩家的 UUID.
-     * @param playtimeData 要设置的玩家游戏时间数据.
+     * @param uuid 玩家的 UUID
+     * @param playtimeData 新的游戏时间数据
      */
     suspend fun setPlaytime(uuid: UUID, playtimeData: PlaytimeData)
 }
